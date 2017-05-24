9a82496ebc40e4943d6ba46dc221e6642a6dded4
c5249526 <eanna.hegerty@sap.com>
2017-05-24 17:09:50 +0100

    added new sidebar create button directive added basic colour scheme added dumpie data inplemented google place search box

--- app/wrapper/wrapper-controller.js
+++ app/wrapper/wrapper-controller.js
@@ -3,27 +3,73 @@ mainApp.controller('wrapper-controller', ['$scope', '$rootScope', 'httpService',
   scope.searchDetails = {
     location: '',
     dish: '',
-    detailed: false
-    
+    showDetailedForm: false,
+    dietrayRequirments: [],
+    filter: '',
+    showMainForm: true
+  };
+  scope.searchResults = {
+    show: false
+  };
+  scope.hi = function(req) {
+   
+    var index = scope.searchDetails.dietrayRequirments.indexOf(req);
+    if (index === -1) {
+      scope.searchDetails.dietrayRequirments.push(req);
+    } else {
+      scope.searchDetails.dietrayRequirments.splice(index, 1);
+    }
+    scope.searchDetails.filter = '';
+  };
+  scope.addOrRemoveRequirements = function(req) {
+    var index = scope.searchDetails.dietrayRequirments.indexOf(req);
+    if (index === -1) {
+      scope.searchDetails.dietrayRequirments.push(req);
+    } else {
+      scope.searchDetails.dietrayRequirments.splice(index, 1);
+    }
+    scope.searchDetails.filter = '';
+  };
 
+  scope.isRequirementChoosen = function(requirement) {
+    return scope.searchDetails.dietrayRequirments.indexOf(requirement);
   };
 
   httpService.getHTTPRequest('app/locations/locations.json', function(list) {
-    scope.locations = list;
+      scope.locations = list;
+    });
+  httpService.getHTTPRequest('app/dietary-requirements/dietary-requirements.json', function(response) {
+    scope.dietaryRequirementList = response.list;
+
   });
-  scope.dietrayRequirments = [];
-  scope.addRequirements = function(req) {
-    console.log('req', scope.dietrayRequirments.indexOf(req));
-    if (scope.dietrayRequirments.indexOf(req) == -1) {
-      scope.dietrayRequirments.push(req);
-    };
+  httpService.getHTTPRequest('app/results/results.json', function(response) {
+    scope.results = response.list;
 
-  };
-  scope.items = ['Gluten', 'Nuts', 'Dairy', 'Celery','Egg', 'Oats', 'Meat', 'Rice', 'Soy', 'Wheat', 'Barley', 'Fish or Shellfish', 'Garlic'];
+  });
 
-  scope.remove =function(req) {
-    
-    var index = scope.dietrayRequirments.indexOf(req);
-    scope.dietrayRequirments.splice(index, 1);
-  };
+    var inputFrom = document.getElementById('location');
+    var autocompleteFrom = new google.maps.places.Autocomplete(inputFrom);
+google.maps.event.addListener(autocompleteFrom, 'place_changed', function() {
+        var place = autocompleteFrom.getPlace();
+        scope.searchDetails.location = place.formatted_address;
+        scope.$apply();
+    });
 }]);
+
+mainApp.directive('requirementButton', function() {
+  
+  return {
+    restrict: 'E',
+    scope: {
+      value: '=',
+      searchDetails:"=",
+      addOrRemoveRequirement:"&",
+      isRequirementChoosen:"&"
+    },
+    templateUrl:'app/directives/button.html',
+    controller: function($scope) {
+     
+    }
+  };
+});
+
